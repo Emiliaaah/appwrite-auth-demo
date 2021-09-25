824f86598ef568aa5c0b9c7d2694066f80292065
Emilia <emilia@emilia.codes>
2021-09-25 02:04:16 +0200

    Added routes for appwrite-drive

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { Switch, Route } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
-import { Login, Register, Dashboard, ForgotPassword, PasswordRecovery } from "./pages"
-
+import { Login, Register, Dashboard, ForgotPassword, PasswordRecovery, Profile } from "./pages"
+import './app.css'
 
 function App() {
   return (
     <Switch>
-      <Route path="/" component={Login} exact />
       <Route path="/password-recovery" component={PasswordRecovery} />
       <PublicRoute path="/login" component={Login} />
       <PublicRoute path="/register" component={Register} />
       <PublicRoute path="/forgot-password" component={ForgotPassword} />
-      <PrivateRoute path="/dashboard" component={Dashboard} />
+      <PrivateRoute path="/profile" component={Profile} />
+      <PrivateRoute path="/" component={Dashboard} exact />
+      <PrivateRoute path="/folder/:folderId" exact component={Dashboard} />
     </Switch>
   );
 }
