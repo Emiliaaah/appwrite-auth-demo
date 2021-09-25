9ae85a535ba926ef7580d1b54231629e9439ace4
Emilia <emilia@emilia.codes>
2021-09-25 02:09:01 +0200

    Added custom context menu

---
+++ src/components/drive/ContextMenu.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useAuth } from "../../contexts/AuthContext";
+
+import useContextMenu from "../hooks/useContextMenu";
+
+const Menu = ({ outerRef, childFiles }) => {
+  const { xPos, yPos, menu, target } = useContextMenu(outerRef);
+  const { deleteFile, deleteDocument } = useAuth()
+
+  function handleClick(e) {
+    const action = e.target.firstChild.data
+    const file = childFiles.filter(file => {
+      if (file.$id === target) return file 
+    })
+    switch (action.toLowerCase()) {
+      case 'open':
+        // eslint-disable-next-line array-callback-return
+        window.location.assign(file[0].viewUrl)
+        break
+
+      case 'download':
+        // eslint-disable-next-line array-callback-return
+        window.open(file[0].downloadUrl, "_blank")
+        break
+
+      case 'delete':
+        deleteFile(file[0].fileId)
+        deleteDocument(process.env.REACT_APP_FILES_COLLECTION_ID, file[0].$id)
+        break
+
+      default:
+        return
+    }
+  }
+
+  if (menu) {
+    return (
+      <ul className="menu" style={{ top: yPos, left: xPos }}>
+        <li onClick={handleClick}>Open</li>
+        <li onClick={handleClick}>Download</li>
+        <li onClick={handleClick}>Delete</li>
+      </ul>
+    );
+  }
+  return <></>;
+};
+
+export default Menu;
---
+++ src/components/hooks/useContextMenu.js
@@ -0,0 +1,40 @@
+import { useEffect, useCallback, useState } from "react";
+
+const useContextMenu = outerRef => {
+  const [xPos, setXPos] = useState("0px");
+  const [yPos, setYPos] = useState("0px");
+  const [menu, showMenu] = useState(false);
+  const [target, setTarget] = useState("")
+
+  const handleContextMenu = useCallback(
+    event => {
+      event.preventDefault();
+      setTarget(event.target.parentNode.id)
+      if (outerRef && outerRef.current.contains(event.target)) {
+        setXPos(`${event.pageX}px`);
+        setYPos(`${event.pageY}px`);
+        showMenu(true);
+      } else {
+        showMenu(false);
+      }
+    },
+    [showMenu, outerRef, setXPos, setYPos]
+  );
+
+  const handleClick = useCallback(() => {
+    showMenu(false);
+  }, [showMenu]);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    document.addEventListener("contextmenu", handleContextMenu);
+    return () => {
+      document.addEventListener("click", handleClick);
+      document.removeEventListener("contextmenu", handleContextMenu);
+    };
+  });
+  
+  return { xPos, yPos, menu, target };
+};
+
+export default useContextMenu;
